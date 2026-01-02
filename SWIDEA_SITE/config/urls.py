04c4d45d1897f2860395ab3b4f3f8394824e22e4
Youngju Lee <oortmealy@gmail.com>
2026-01-02 21:44:06 +0900

    [fix] : 메인 url 수정

--- SWIDEA_SITE/config/urls.py
+++ SWIDEA_SITE/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/idea/', permanent=False)),
     path('admin/', admin.site.urls),
     path('idea/', include('idea.urls')),
     path('tool/', include('tool.urls')),
